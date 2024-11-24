35fa3af7e2aac4b2764072846cd1ccf7cf60d22f
Pooja Bastawadi <poojabastawadi7@gmail.com>
2024-11-24 17:19:29 +0530

    Added NavBar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+  const [showSidebar, setShowSidebar] = useState(false)
+  return (
+    <div className='navbar container'>
+        <a href="!#" className='logo'>RecipeHub</a>
+        <div className='nav-links'>
+            <a href="!#">Home</a>
+            <a href="!#">Recipes</a>
+            <a href="!#">Settings</a>
+        </div>
+        <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"} >
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar;
